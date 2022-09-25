379205c81849d082bab0ba6d9d08f37688b6eebf
GuiGui <110209962+GuiGui0v0@users.noreply.github.com>
2022-09-26 00:20:25 +1000

    111

--- sketch.js
+++ sketch.js
@@ -1,9 +1,33 @@
-function setup() 
-{
-	createCanvas(400, 400);
+var song
+var fft
+
+function preload() {
+	song = loadSound('Stay_320(PaglaSongs).mp3')
 }
 
-function draw()
-{
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	fft = new p5.FFT()
+}
 
+function draw() {
+	background(0)
+	stroke(255)
+
+	var wave = fft.waveform ()
+
+	for (var i=0; i<width; i++){
+		var index = floor(map(i,0,width,0,wave.length))
+		var x=i
+		var y=wave[index]*300 + height / 2
+		point(x,y)
+	}
+}
+
+function mouseClicked() {
+	if(song.isPlaying()){
+		song.pause
+	} else {
+		song.play()
+	}
 }
